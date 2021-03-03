71a89d362eac4d52b25834bf9861723c1d71a9b4
Mauricio van der Maesen de Sombreff <mauricio@n2048.com>
2021-03-03 12:14:19 +0100

    smooth vortex mirror and different times per mirror position

--- arduino-code/vortex-arduino/vortex-arduino.ino
+++ arduino-code/vortex-arduino/vortex-arduino.ino
@@ -3,12 +3,14 @@
 #define VORTEX_PIN 11
 #define MIRROR_PIN 6
 
-int vortexCounterMax = 2; // seconds
-int vortexValues[] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; // %
+int vortexCounterMax = 5; // seconds
+int vortexValues[] = {100, 100, 100, 100, 100, 100, 0}; // %
 
-int mirrorValues[] = {0, 180}; // ~degrees // CHANGE THIS VALUES IN PLACE
-int mirrorValueIndex = 0;
+//95 -> canvas
+//120 -> center
 
+int mirrorValues[] = {85, 120}; // ~degrees // CHANGE THIS VALUES IN PLACE
+int mirrorTimes[] = {10, 30}; // seconds // rotate
 
 ServoTimer2 mirrorServo;
 
@@ -16,21 +18,20 @@ volatile int vortexCounter = 0;
 int vortexValueIndex = 0;
 
 volatile int mirrorCounter = 0;
-int mirrorCounterMax = 5; // seconds
+int mirrorValueIndex = 0;
 
 volatile int counter = 0;
 
-int maxVortexIndex = (sizeof(vortexValues) / sizeof(vortexValues[0])) -1;
-int maxMirrorIndex = (sizeof(mirrorValues) / sizeof(mirrorValues[0])) -1;
+int maxVortexIndex = (sizeof(vortexValues) / sizeof(vortexValues[0])) - 1;
+int maxMirrorIndex = (sizeof(mirrorValues) / sizeof(mirrorValues[0])) - 1;
 
 void setup() {
 
   cli();//stop interrupts
-//  Serial.begin(9600);
+  //  Serial.begin(9600);
 
-  pinMode(VORTEX_PIN, OUTPUT);  
+  pinMode(VORTEX_PIN, OUTPUT);
   mirrorServo.attach(MIRROR_PIN);  // attaches the servo on pin 9 to the servo object
-
 
   //set timer1 interrupt at 1Hz
   TCCR1A = 0;// set entire TCCR1A register to 0
@@ -52,7 +53,7 @@ void loop () {
 
   // Mirror Update:
 
-  if (mirrorCounter > mirrorCounterMax) {
+  if (mirrorCounter > mirrorTimes[mirrorValueIndex]) {
     mirrorCounter = 0;
     mirrorValueIndex += 1;
     if (mirrorValueIndex > maxMirrorIndex) {
@@ -60,9 +61,18 @@ void loop () {
     }
   }
 
-  int mirrorPos = map(mirrorValues[mirrorValueIndex], 0, 180, 750, 2250);
-  mirrorServo.write(mirrorPos);
+  int targetMirrorPos = map(mirrorValues[mirrorValueIndex], 0, 180, 750, 2250);
+  float currMirrorPos = mirrorServo.read();
 
+  //  Serial.print(targetMirrorPos);
+  //  Serial.print(" - ");
+  //  Serial.println(currMirrorPos);
+
+  while (targetMirrorPos != currMirrorPos) {
+    currMirrorPos += abs(targetMirrorPos - currMirrorPos) / (targetMirrorPos - currMirrorPos);
+    mirrorServo.write(int(currMirrorPos));
+    delay(4);
+  }
 
   // Vortex Update:
   if (vortexCounter > vortexCounterMax) {
@@ -77,7 +87,7 @@ void loop () {
   int vortexSpeed = map(vortexValues[vortexValueIndex], 0, 100, 0, 255);
   analogWrite(VORTEX_PIN, vortexSpeed);
 
-//  Serial.println(vortexSpeed);
+  //  Serial.println(vortexSpeed);
   delay(2);
 }
 
